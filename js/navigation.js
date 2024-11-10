ba33299a61bb41ae137c5242d11eca5831ea76d6
Adam Mladý <chickpop24@gmail.com>
2024-11-11 00:18:36 +0100

    doing stuff here also

---
+++ js/navigation.js
@@ -0,0 +1,17 @@
+const options = {
+    behavior: "smooth"
+};
+
+function customMixes() { document.getElementById("custom-mixes").scrollIntoView(options); }
+
+function testedLiked() { document.getElementById("tested-liked").scrollIntoView(options); }
+
+function testedDisliked() {}
+
+function notTestedYet() {}
+
+function iceFlavors() {}
+
+function longFills() {}
+
+function vapeLiquids() {}
